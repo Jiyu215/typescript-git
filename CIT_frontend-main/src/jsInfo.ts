89543933ee30ec0e74608eefb87dc761f47210cc
Jiyu215 <jinyu010512@gmail.com>
2024-03-04 15:44:42 +0900

    chore: 프로젝트 파일 가져오기(#2)

---
+++ CIT_frontend-main/src/jsInfo.ts
@@ -0,0 +1,15 @@
+export const name = 'CIT';
+
+export const getName = () => {
+    return `제 이름은 ${name}입니다.`;
+}
+
+export const getSum = (param: number) => {
+    const number1 = 3;
+    const number2 = 4;
+
+    return number1 + number2 + param;
+}
+
+/*동작확인을 위한 주석*/
+/*동작확인을 위한 주석2*/
